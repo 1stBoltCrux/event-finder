5cd0e8da4f90396e61fb90d3f9e8116ac4bd0bd4
Aaron Akbari-Mort and Ted Leary <ted@>
2018-04-09 16:04:13 -0700

    country musician objects

--- js/scripts.js
+++ js/scripts.js
@@ -13,8 +13,6 @@ function Venue(name, location) {
   this.location = location;
 }
 
-function Bands()
-
 var wonderBallroom = new Venue("Wonder Ballroom", "Northeast");
 var theKnow = new Venue("The Know", "Northeast");
 var mississippiStudios = new Venue("Mississippi Studios", "Northeast");
@@ -26,3 +24,10 @@ var analogCafe = new Venue("The Analaog Cafe", "Southeast");
 var crystalBallroom = new Venue("The Crystal Ballroom", "Southwest");
 var dantes = new Venue("Dante's", "Southwest");
 var schnitzer = new Venue("The Arlene Schnitzer Concert Hall", "Southwest");
+
+var bradPaisley = new Event(["Brad Paisley"], [2018, 4, 2] "7:00 PM", crystalBallroom, "aa", "Country", 45);
+var dariusRucker = new Event(["Darius Rucker"], [2018, 4, 4], "7:00 PM", schnitzer, "21", "Country", 70);
+var dierksBently = new Event(["Dierks Bently"], [2018, 4, 6], "7:00 PM", roselandTheater, "aa", "Country", 35);
+var keithUrban = new Event(["Keith Urban"], [2018, 4, 8], "8:00 PM", wonderBallroom, "21", "Country", 50);
+var mirandaLambert = var Event(["Miranda Lambert"], [2018, 4, 10], "6:00 PM", starTheater, "aa", "Country", 35);
+var rascalFlats = new Event(["Rascal Flats"], [2018, 4, 12], "7:00 PM", wonderBallroom, "aa", "Country", 25);
