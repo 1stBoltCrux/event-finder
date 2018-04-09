9e93261a31c9c4cef571dc62857b8e5947f448b2
Matt Dowdney and matt miller <matt@>
2018-04-09 15:43:43 -0700

    add js variables for form data and implement search

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,38 @@
+//Back-end Logic
+var concertArray = [];
 
 
 
 
 
+//rewrite as prototype?
+function concertSearch(number){
+  for (var i = 0; i < concertArray.length; i++) {
+    if (concertArray[i].date === inputDateToNumber){
+      return [i];
+    }
+  }
+}
 
+var inputDateToNumber = [];
 
+function dateToNumber(array){
+  array.forEach(function(element){
+    inputDateToNumber.push(parseInt(element))
+  });
+}
 
-
-
-
-
-
+//UI Logic
 $(document).ready(function(){
   $("#properties-search").submit(function(event){
-    
+  event.preventDefault();
+    var inputDate = $("#date").val().split("-");
+    dateToNumber(inputDate);
+    var inputArea = $("#area").val();
+    var inputGenre = $("#genre").val();
+    var inputAge = $("#allage").val();
 
+    console.log(inputDateToNumber);
 
 
 
